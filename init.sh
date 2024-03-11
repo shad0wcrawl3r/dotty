00a724a403a8fb0ce6d50a1e6ca810090454eae0
Ashwin Belbase <ashwin@teslatech.com.np>
2024-03-11 11:33:45 +0545

    Implement init for alacritty

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-
+set -e
 # Configure EWW if eww is preinstalled
 if [[ $(which eww) ]]; then
 	if [[ -d ~/.config/eww/ ]]; then
@@ -8,4 +8,13 @@ if [[ $(which eww) ]]; then
 	fi
 	ln -sv $(pwd)/eww ~/.config/eww
 	eww open mainBar
+fi
+
+# COnfigure alacritty if installed
+if [[ $(which alacritty) ]]; then
+	git submodule update --init alacritty/catppuccin
+	if [[ -d ~/.config/alacritty ]]; then
+		mv -v ~/.config/alacritty{,.bak}
+	fi
+	ln -sv $(pwd)/alacritty ~/.config/alacritty
 fi
