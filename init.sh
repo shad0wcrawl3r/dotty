dc4f9e1dff4ee3f6087c0159500c4cc336eb788a
Ashwin Belbase <ashwinbelbase@gmail.com>
2024-04-29 10:48:58 +0545

    Add dashboard and some scripts

--- init.sh
+++ init.sh
@@ -7,9 +7,41 @@ function backup {
 		mv -v $1{,.$(date +%s).bak}
 	fi
 }
-set -e
+set -xe
 OS_ID=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2)
 OS_NAME=$(cat /etc/os-release | grep "^NAME=" | cut -d= -f2)
+
+# For NixOS based hosts
+function deploy_nix_config() {
+	echo "Running dmidecode to detect system information"
+	dmidecode_out=$(nix-shell -p dmidecode --run "sudo dmidecode -s system-product-name")
+	[[ $dmidecode_out =~ "FA507RM" ]] && device_hostname_should_be="wrath"
+	[[ $dmidecode_out =~ "GA503QM" ]] && device_hostname_should_be="CrYbAbY58"
+	if [[ $(grep $device_hostname_should_be /etc/nixos/flake.nix) ]]; then
+		echo "Buidling with the $device_hostname_should_be profile"
+		[[ $EUID == 0 ]] || echo "Building with Effective User ID (EUID) = $EUID. Might fail "
+		nixos-rebuild switch
+	else
+		echo "Cannot find the expected hostname in flake.nix. Exiting..."
+		exit 1
+	fi
+}
+
+function first_nix_setup() {
+	if [[ $EUID != 0 ]]; then
+		echo "Effective User ID is not 0. Rerun the script as root"
+		exit 1
+	fi
+	if [[! $(which sbctl) ]]; then
+		echo "No sbctl found. Assuming Fist configuration. Sleeping for 10s to allow for forced exiting."
+		sleep 10
+		nix-shell -p sbctl --command sbctl create-keys
+		deploy_nix_config
+		nix-shell -p sbctl --command sbctl verify
+		echo "It is expected some files are not signed. Crosscheck with the documentation if anything is not working."
+		echo "https://github.com/nix-community/lanzaboote/blob/master/docs/QUICK_START.md"
+	fi
+}
 
 ## For Hyprland ecosystem
 function hypr() {
@@ -31,42 +63,45 @@ function hypr() {
 # 	nixos-rebuild switch
 # 	exit 0
 # fi
+function main() {
+	if [[ $(which hyprctl) ]]; then
+		hypr
+	fi
 
-if [[ $(which hyprctl) ]]; then
-	hypr
-fi
+	# Configure EWW if eww is preinstalled
+	if [[ $(which eww) ]]; then
+		config_path=~/.config/eww
+		backup $config_path
+		ln -sv $(pwd)/eww $config_path
+		eww open mainBar
+	fi
 
-# Configure EWW if eww is preinstalled
-if [[ $(which eww) ]]; then
-	config_path=~/.config/eww
-	backup $config_path
-	ln -sv $(pwd)/eww $config_path
-	eww open mainBar
-fi
+	# COnfigure alacritty if installed
+	if [[ $(which alacritty) ]]; then
+		config_path=~/.config/alacritty
+		git submodule update --init alacritty/catppuccin
+		backup $config_path
+		ln -sv $(pwd)/alacritty $config_path
+	fi
 
-# COnfigure alacritty if installed
-if [[ $(which alacritty) ]]; then
-	config_path=~/.config/alacritty
-	git submodule update --init alacritty/catppuccin
-	backup $config_path
-	ln -sv $(pwd)/alacritty $config_path
-fi
+	# Configure Rofi if installed
+	if [[ $(which rofi) ]]; then
+		config_path=~/.config/rofi
+		backup $config_path
+		ln -sv $(pwd)/rofi $config_path
+	fi
 
-# Configure Rofi if installed
-if [[ $(which rofi) ]]; then
-	config_path=~/.config/rofi
-	backup $config_path
-	ln -sv $(pwd)/rofi $config_path
-fi
+	#Configure neovim if installed
+	if [[ $(which nvim) ]]; then
+		config_path=~/.config/nvim
+		git submodule update --init neovim
+		backup $config_path
+		rm -vrf ~/.local/share/nvim/
+		rm -vrf ~/.local/state/nvim/
+		rm -vrf ~/.cache/nvim{,.bak}
+		ln -sv $(pwd)/neovim $config_path
+	fi
 
-#Configure neovim if installed
-if [[ $(which nvim) ]]; then
-	config_path=~/.config/nvim
-	git submodule update --init neovim
-	backup $config_path
-	rm -vrf ~/.local/share/nvim/
-	rm -vrf ~/.local/state/nvim/
-	rm -vrf ~/.cache/nvim{,.bak}
-	ln -sv $(pwd)/neovim $config_path
-fi
+}
 
+deploy_nix_config
